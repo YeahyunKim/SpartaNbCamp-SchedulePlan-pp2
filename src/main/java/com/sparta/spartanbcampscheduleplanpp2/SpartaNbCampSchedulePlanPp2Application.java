b329d0606ddbe687a8063417838b8de802dcabc4
Yeahyun Kim <insidesy4@gmail.com>
2024-05-27 12:12:24 +0900

    [Add] 파일 추가

---
+++ src/main/java/com/sparta/spartanbcampscheduleplanpp2/SpartaNbCampSchedulePlanPp2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spartanbcampscheduleplanpp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaNbCampSchedulePlanPp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaNbCampSchedulePlanPp2Application.class, args);
+    }
+
+}
