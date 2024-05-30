755d3a13592d0bbbca6ead5faa165665ba91abb5
Yeahyun Kim <insidesy4@gmail.com>
2024-05-30 18:11:23 +0900

    [Feat] level 5 validation으로 예외처리 메세지 로그에 띄워보기 #14

--- src/main/java/com/sparta/spartanbcampscheduleplanpp2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/spartanbcampscheduleplanpp2/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sparta.spartanbcampscheduleplanpp2.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,13 +15,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+@Slf4j(topic = "Validation 에러메세지 츨력")
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> exampleResponseValidation(MethodArgumentNotValidException e) {
         Map<String, String> error = new HashMap<>();
         e.getAllErrors().forEach(
-                c -> error.put(((FieldError) c).getField(), c.getDefaultMessage())
+                c -> {
+                    error.put(((FieldError) c).getField(), c.getDefaultMessage());
+                    log.error(c.getDefaultMessage());
+                }
         );
         return ResponseEntity.badRequest().body(error);
     }
