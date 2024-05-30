9095b9a4a614a12c43b18ee1a4ddee87c3ed6ffe
Yeahyun Kim <insidesy4@gmail.com>
2024-05-30 14:38:55 +0900

    [Feat] level 2-2 선택한 일정의 ID를 입력 받지 않은 경우 예외처리 #4

--- src/main/java/com/sparta/spartanbcampscheduleplanpp2/service/CommentService.java
+++ src/main/java/com/sparta/spartanbcampscheduleplanpp2/service/CommentService.java
@@ -8,6 +8,7 @@ import com.sparta.spartanbcampscheduleplanpp2.entity.Schedule;
 import com.sparta.spartanbcampscheduleplanpp2.repository.CommentRepository;
 import com.sparta.spartanbcampscheduleplanpp2.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class CommentService {
@@ -20,6 +21,11 @@ public class CommentService {
     }
 
     public CommentResponseDto createComment(CommentReqeustDto reqeustDto) {
+        //코멘트 예외처리 : 선택한 일정의 scheduleId를 입력받지 않은 경우
+        if (!StringUtils.hasText(reqeustDto.getScheduleId().toString())) {
+            throw new IllegalArgumentException("해당 아이디가 존재하지 않습니다.");
+        }
+
         Schedule scheduleById = scheduleRepository.findById(reqeustDto.getScheduleId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 스케줄이 존재하지 않습니다. id=" + reqeustDto.getScheduleId()));
 
